1f4bffb43b985c3643ca400fb1e95dd65c3941bb
mdasadul <mdasadul@ualberta.ca>
2016-01-20 14:09:47 -0600

    adding traversing both way

--- src/Insert.java
+++ src/Insert.java
@@ -1,18 +1,33 @@
 import java.util.*;
+import java.util.stream.*;
 
 public class Insert {
 
-	LinkedList<Integer> firstList = new LinkedList<Integer>(); 
-	void insert(int item){
-		
+	static LinkedList<Integer> firstList = new LinkedList<Integer>();
+
+	private void insert(int item) {
+
 		firstList.add(item);
+
 	}
-	void traverse(){
-		for(int item:firstList){
-			System.out.println(item);
+
+	private void traverse() {
+		firstList.forEach(System.out::println);
+
+		Iterator<Integer> x = firstList.descendingIterator();
+		while (x.hasNext()) {
+			System.out.print(x.next() + " ");
 		}
+
 	}
-	public static void main(String args[]){
-		
+
+	public static void main(String args[]) {
+
+		Insert insertObject = new Insert();
+		IntStream.range(1, 10).forEach(item -> {
+			insertObject.insert(item);
+		});
+		insertObject.traverse();
 	}
+
 }
