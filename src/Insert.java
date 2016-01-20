22474a6e6373f64ad9bc36aa861fe1875289ecf5
mdasadul <mdasadul@ualberta.ca>
2016-01-20 13:13:15 -0600

    adding insert and traverse function

--- src/Insert.java
+++ src/Insert.java
@@ -1,4 +1,18 @@
+import java.util.*;
 
 public class Insert {
 
+	LinkedList<Integer> firstList = new LinkedList<Integer>(); 
+	void insert(int item){
+		
+		firstList.add(item);
+	}
+	void traverse(){
+		for(int item:firstList){
+			System.out.println(item);
+		}
+	}
+	public static void main(String args[]){
+		
+	}
 }
